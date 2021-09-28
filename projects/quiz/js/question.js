c217500b2acde604583f00aae7acd703e4201868
Aleksandar Zdravkovic <zdravkovic92@yahoo.com>
2021-09-28 14:20:00 +0200

    Quiz

---
+++ projects/quiz/js/question.js
@@ -0,0 +1,120 @@
+var questions = [{
+    "question": "Koji je glavni grad Srbije?",
+    "option1": "Beograd",
+    "option2": "Sarajevo",
+    "option3": "Zagreb",
+    "option4": "Podgorica",
+    "answer": "1" 
+},
+{
+    "question": "Koji je glavni grad Nemacke?",
+    "option1": "Pariz",
+    "option2": "Berlin",
+    "option3": "Bec",
+    "option4": "London",
+    "answer": "2"
+},
+{
+    "question": "Koji je glavni grad Italije?",
+    "option1": "Moskva",
+    "option2": "Bec",
+    "option3": "Los Angeles",
+    "option4": "Rim",
+    "answer": "4"
+},
+{
+    "question": "Koji je glavni grad Rumunije?",
+    "option1": "Bukurest",
+    "option2": "Kiev",
+    "option3": "Zagreb",
+    "option4": "Podgorica",
+    "answer": "1"
+},
+{
+    "question": "Koji je glavni grad Engleske?",
+    "option1": "Cikago",
+    "option2": "Moskva",
+    "option3": "London",
+    "option4": "Amsterdam",
+    "answer": "3"
+},
+{
+    "question": "Koji je glavni grad Albanije?",
+    "option1": "Tirana",
+    "option2": "Detroit",
+    "option3": "Split",
+    "option4": "Novi Sad",
+    "answer": "1"
+},
+{
+    "question": "Koji je glavni grad Svedske?",
+    "option1": "Malmo",
+    "option2": "Stockholm",
+    "option3": "Helsingborg",
+    "option4": "Orebro",
+    "answer": "2"
+},
+{
+    "question": "Koji je glavni grad Norveske?",
+    "option1": "Trondheim",
+    "option2": "Oslo",
+    "option3": "Bergen",
+    "option4": "Amsterdam",
+    "answer": "2"
+},
+{
+    "question": "Koji je glavni grad Holandije?",
+    "option1": "Amsterdam",
+    "option2": "Rim",
+    "option3": "Atina",
+    "option4": "Bergen",
+    "answer": "1"
+},
+{
+    "question": "Koji je glavni grad Grcke?",
+    "option1": "Rim",
+    "option2": "Beograd",
+    "option3": "Atina",
+    "option4": "Amsterdam",
+    "answer": "3"
+},
+{
+    "question": "Koji je glavni grad Kanade?",
+    "option1": "Toronto",
+    "option2": "Bec",
+    "option3": "Pariz",
+    "option4": "Otava",
+    "answer": "4"
+},
+{
+    "question": "Koji je glavni grad Australije?",
+    "option1": "Rim",
+    "option2": "Melbourne",
+    "option3": "Canberra",
+    "option4": "Sydney",
+    "answer": "3"
+},
+{
+    "question": "Koji je glavni grad Kine?",
+    "option1": "Beijing",
+    "option2": "Bogota",
+    "option3": "Moroni",
+    "option4": "Prague",
+    "answer": "1"
+},
+{
+    "question": "Koji je glavni grad Kube?",
+    "option1": "Santo Domingo",
+    "option2": "Havana",
+    "option3": "Doha",
+    "option4": "Lima",
+    "answer": "2"
+},
+{
+    "question": "Koji je glavni grad Saudijske Arabije?",
+    "option1": "Khartoum",
+    "option2": "Madrid",
+    "option3": "Bern",
+    "option4": "Riyadh",
+    "answer": "4"
+}]
